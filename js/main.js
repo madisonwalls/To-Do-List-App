8540ad2d3b9b320407260d8eae6a5e06221e15a4
madisonwalls <madilyn@live.unc.edu>
2017-01-27 14:09:50 -0500

    Initial Commit. Some changes made to original file. Added Vue elements

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  data: {
+
+    todoTitle: '',
+    todoContent: '',
+    item: []
+  },
+
+  methods: {
+
+    newCard: function() {
+
+    this.item.push ({
+      content: this.todoTitle
+    });
+
+    this.todoTitle='';
+
+    }
+
+  },
+});
